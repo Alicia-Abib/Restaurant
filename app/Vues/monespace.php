ee4e3238913e908559cd797d9d855336deea7801
Alicia Abib <abib.alicia@yahoo.com>
2025-04-21 21:41:44 +0200

    updates

--- app/Vues/monespace.php
+++
@@ -1,8 +0,0 @@
-<h2>🎟️ Accéder à vos réservations</h2>
-
-<form method="GET" action="">
-    <input type="hidden" name="url" value="Reservation/mesReservations">
-    <label>Entrez votre adresse email :</label>
-    <input type="email" name="email" required>
-    <button type="submit">Voir mes réservations</button>
-</form>
